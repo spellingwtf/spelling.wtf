4f325d23b00163b2cdead06a69c5e95026875ba0
spellingwtf <99119335+spellingwtf@users.noreply.github.com>
2022-02-17 21:54:13 +0100

    thing

--- assets/js/random_image.js
+++ assets/js/random_image.js
@@ -1,23 +1,7 @@
 window.onload = displayImage;
 
-var imagesArray = [
-    "anime1.jpg", 
-    "anime2.jpeg", 
-    "anime3.png", 
-    "anime4.png", 
-    "anime5.png", 
-    "anime6.jpg", 
-    "anime7.png", 
-    "anime8.jpg", 
-    "anime9.gif",
-    "anime10.jpg",
-    "anime11.jpg",
-    "anime12.jpg",
-    "anime13.jpg",
-    "anime14.jpg",
-    "anime15.jpg",
-    "anime16.gif",
-    "anime17.jpg"
+var imagesArray = [ 
+    "anime4.png"
 ];
 
 function displayImage(){
