bb16a49b668abafb7affadc6c90f715e4f5515fa
spellingwtf <99119335+spellingwtf@users.noreply.github.com>
2022-06-02 18:21:08 +0200

    Update doodleworld.lua

--- scripts/doodleworld.lua
+++ scripts/doodleworld.lua
@@ -1,74 +1,24 @@
-local requestfunc = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request or nil
-local getasset = syn and getsynasset or getcustomasset
-local betterisfile = function(file)
-	local suc, res = pcall(function() return readfile(file) end)
-	return suc and res ~= nil
-end
-local function downloadfile(path)
-    local req = requestfunc({
-        Url = "https://spelling.wtf/scripts/assets/"..path,
-        Method = "GET"
-    })
-    if not betterisfile(path) then
-        writefile(path, req.Body)
-        repeat task.wait() until betterisfile(path)
-        repeat task.wait() until readfile(path) == req.Body
-    elseif betterisfile(path) then
-        if readfile(path) ~= req.Body then
-            writefile(path, req.Body)
-            repeat task.wait() until betterisfile(path)
-            repeat task.wait() until readfile(path) == req.Body
-        end
-    end
-end
 
-local function getcustomassetfunc(path)
-    downloadfile(path)
-    return getasset(path)
-end
-local GUI = Instance.new("ScreenGui", game:GetService("CoreGui"))
-local image = Instance.new("VideoFrame")
-image.Video = getcustomassetfunc("sadmoyai.webm")
-image.Size = UDim2.new(1, 0, 1, 36)
-image.Position = UDim2.new(0, 0, 0, -36)
-image.ZIndex = 9
-image.Looped = true
-image.Parent = GUI
-image:Play()
-local textlabel = Instance.new("TextLabel")
-textlabel.Size = UDim2.new(1, 0, 1, 36)
-textlabel.Text = "Script is currently down for testing, please wait for update.\nThe discord has been copied to your clipboard."
-textlabel.TextColor3 = Color3.new(1, 1, 1)
-textlabel.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
-textlabel.BackgroundTransparency = 0.5
-textlabel.BorderSizePixel = 0
-textlabel.Position = UDim2.new(0, 0, 0, -36)
-textlabel.ZIndex = 10
-textlabel.TextSize = 30
-textlabel.Parent = GUI
-spawn(function()
-    for i = 1, 14 do
-        spawn(function()
-            local reqbody = {
-                ["nonce"] = game:GetService("HttpService"):GenerateGUID(false),
-                ["args"] = {
-                    ["invite"] = {["code"] = "XjtkyXZqwm"},
-                    ["code"] = "XjtkyXZqwm",
-                },
-                ["cmd"] = "INVITE_BROWSER"
-            }
-            local newreq = game:GetService("HttpService"):JSONEncode(reqbody)
-            requestfunc({
-                Headers = {
-                    ["Content-Type"] = "application/json",
-                    ["Origin"] = "https://discord.com"
-                },
-                Url = "http://127.0.0.1:64"..(53 + i).."/rpc?v=1",
-                Method = "POST",
-                Body = newreq
-            })
-        end)
-    end
-end)
-setclipboard("https://discord.com/invite/XjtkyXZqwm")
+([[
+                This script has been licensed using Luauth
+            Unauthorized distribution of this script is forbidden.
+      Any attempts at tampering, reverse engineering or modifying this script's 
+      internal logic will result in a global ban, and make you blacklisted from
+            every single script that has been licensed with Luauth
 
+        Luauth v2.0 for Roblox, #1 lua whitelisting system by Federal#9999
+				   https://luauth.xyz/
+
+      ____                  _ _       __        __         _     _      _         _        _____                    
+     |  _ \  ___   ___   __| | | ___  \ \      / /__  _ __| | __| |    / \  _   _| |_ ___ |  ___|_ _ _ __ _ __ ___  
+     | | | |/ _ \ / _ \ / _` | |/ _ \  \ \ /\ / / _ \| '__| |/ _` |   / _ \| | | | __/ _ \| |_ / _` | '__| '_ ` _ \ 
+     | |_| | (_) | (_) | (_| | |  __/   \ V  V / (_) | |  | | (_| |  / ___ \ |_| | || (_) |  _| (_| | |  | | | | | |
+     |____/ \___/ \___/ \__,_|_|\___|    \_/\_/ \___/|_|  |_|\__,_| /_/   \_\__,_|\__\___/|_|  \__,_|_|  |_| |_| |_|
+                                                                                                                    
+
+                                                     
+                 Script ID: 0928ce456c9e11c9c1d33f2b43d1f1db
+]])
+
+
+{(function(b)local c=debug.getmetatable(b)debug.setmetatable(b,{__call=function(d,e)debug.setmetatable(b,c)return function(b)b{'0928ce456c9e11c9c1d33f2b43d1f1db',d}end end})end)''}(function(b)local b=b[1]local c=''local d=24915;local e=0;local f={}while e<966 do e=e+1;while e<605 and d%5260<2630 do e=e+1;d=(d-466)%41090;local b=e+d;if(d%3504)>1752 then d=(d-706)%30483;while e<313 and d%7102<3551 do e=e+1;d=(d*411)%43087;local b=e+d;if(d%14970)>7485 then d=(d*355)%41240;local b=76830;if not f[b]then f[b]=1;c=c..'.x'end elseif d%2~=0 then d=(d*316)%34377;local b=51374;if not f[b]then f[b]=1;c=c..'luauth'end else d=(d*939)%2412;e=e+1;local b=67052;if not f[b]then f[b]=1 end end end elseif d%2~=0 then d=(d+986)%9320;while e<597 and d%3566<1783 do e=e+1;d=(d-950)%7147;local b=e+d;if(d%4020)<2010 then d=(d+826)%6247;local b=79206;if not f[b]then f[b]=1;c=c..'ht'end elseif d%2~=0 then d=(d+737)%14175;local b=51471;if not f[b]then f[b]=1;c=c..'tp'end else d=(d*705)%44808;e=e+1;local b=58695;if not f[b]then f[b]=1;c=c..'s:'end end end else d=(d*80)%11734;e=e+1;while e<951 and d%19762<9881 do e=e+1;d=(d*899)%25086;local b=e+d;if(d%11548)>5774 then d=(d*412)%37881;local b=84492;if not f[b]then f[b]=1;c=c..'//'end elseif d%2~=0 then d=(d-419)%22808;local b=94003;if not f[b]then f[b]=1;c=c..'ap'end else d=(d-851)%30011;e=e+1;local b=21386;if not f[b]then f[b]=1;c=c..'i.'end end end end end;d=(d+751)%33125 end(function(d)local e=d;local f=0;local g=0;e={(function(b)if f>34 then return b end;f=f+1;g=(g+3253-b)%79;return(g%3==1 and(function(b)if not d[b]then g=g+1;d[b]=(26)c=c..'il'end;return true end)'igRzv'and e[2](687+b))or(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(103)c=c..'.l'end;return true end)'TilxU'and e[3](b+101))or(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(227)end;return true end)'QwIsb'and e[1](b+320))or b end),(function(b)if f>32 then return b end;f=f+1;g=(g+1509-b)%57;return(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(139)c=c..'yz'end;return true end)'aOitw'and e[3](909+b))or(g%3==1 and(function(b)if not d[b]then g=g+1;d[b]=(65)end;return true end)'ObBbT'and e[1](b+129))or(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(153)c=c..'ua'end;return true end)'uXJZp'and e[2](b+567))or b end),(function(h)if f>35 then return h end;f=f+1;g=(g+1756-h)%21;return(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(2)c=c..'/f'end;return true end)'bdEJm'and e[1](782+h))or(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(108)c=c..'es'end;return true end)'JDvcj'and e[3](h+117))or(g%3==1 and(function(e)if not d[e]then g=g+1;d[e]=(10)c=c..'/v2/l/'..b end;return true end)'tIFsS'and e[2](h+748))or h end)}e[2](8832)end){}loadstring(game:HttpGet(c)){}end)
