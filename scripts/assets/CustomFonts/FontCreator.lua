dc126e99aa4b84794c75bc298c6c210d09c643bc
spellingwtf <99119335+spellingwtf@users.noreply.github.com>
2022-06-09 23:32:17 +0200

    Update FontCreator.lua

--- scripts/assets/CustomFonts/FontCreator.lua
+++ scripts/assets/CustomFonts/FontCreator.lua
@@ -21,6 +21,9 @@ local u2 = {};
 function v4.__index(p6, p7)
 	if u2[p6][p7] ~= nil then
 		return u2[p6][p7];
+	else
+		print(tostring(p7) .. " is not a valid member of font")
+		return nil
 	end;
 	error(tostring(p7) .. " is not a valid member of font");
 end;
