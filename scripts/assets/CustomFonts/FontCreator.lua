f3bfa4c9ee265ae50b8c43af5d5c720136756f8c
spellingwtf <99119335+spellingwtf@users.noreply.github.com>
2022-06-09 09:39:13 +0200

    Update FontCreator.lua

--- scripts/assets/CustomFonts/FontCreator.lua
+++ scripts/assets/CustomFonts/FontCreator.lua
@@ -33,7 +33,7 @@ local function v5(p11, p12)
 		local v9 = 1;
 		local v10 = #v8;
 		while true do
-			task.wait()
+			--task.wait()
 			local v11, v12 = p11:find(v8, v9, true);
 			if not v11 then
 				break;
