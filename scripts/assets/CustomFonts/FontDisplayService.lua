bc636ba859251680f53fb32c49ef54ecd86e28cb
spellingwtf <99119335+spellingwtf@users.noreply.github.com>
2022-06-09 22:30:18 +0200

    Update FontDisplayService.lua

--- scripts/assets/CustomFonts/FontDisplayService.lua
+++ scripts/assets/CustomFonts/FontDisplayService.lua
@@ -506,7 +506,7 @@ function u1.Write(p27, p28)
 		if v129 == nil then
 			v129 = true;
 		end;
-		return p27:WriteToFrame(p29.Font, p29.Size or p29.Frame.AbsoluteSize.Y, p28, p29.Wraps and false, p29.Frame, v129, p29);
+		return p27:WriteToFrame(p29.Font, p29.Size or p29.Frame.AbsoluteSize.Y, p28, p29.Wraps, p29.Frame, v129, p29);
 	end;
 end;
 return v1;
