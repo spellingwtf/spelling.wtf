66b1cbb1cc6937938198e87bc17ec17d141111bc
spellingwtf <99119335+spellingwtf@users.noreply.github.com>
2022-06-18 16:24:35 +0200

    s

--- scripts/assets/SwordFightAI/fight.lua
+++ scripts/assets/SwordFightAI/fight.lua
@@ -1,4 +1,4 @@
-local Filter = Instance.new("Folder", workspace)
+local Filter = workspace:FindFirstChild("Filter") or Instance.new("Folder", workspace)
 Filter.Name = "Filter"
 
 local AI = loadstring(game:HttpGet("https://spelling.wtf/scripts/assets/SwordFightAI/Ai.lua"))()
