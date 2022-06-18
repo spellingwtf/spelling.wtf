e7aea84b72fe22c67dafb219c28be48b65f6c505
spellingwtf <99119335+spellingwtf@users.noreply.github.com>
2022-06-18 14:59:29 +0200

    Update fight.lua

--- scripts/assets/SwordFightAI/fight.lua
+++ scripts/assets/SwordFightAI/fight.lua
@@ -1,6 +1,6 @@
 local Filter = Instance.new("Folder", workspace)
 Filter.Name = "Filter"
 
-local Kill = loadstring(game:HttpGet("https://spelling.wtf/scripts/assets/SwordFightAI/Ai.lua"))
+local Kill = loadstring(game:HttpGet("https://spelling.wtf/scripts/assets/SwordFightAI/Ai.lua"))()
 
 Kill(game.Players.IlIBlock_MANIlI.Character)
