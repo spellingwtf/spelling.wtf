b8372b649ebf388be32bfc67e078c7d064ec02c4
spellingwtf <99119335+spellingwtf@users.noreply.github.com>
2023-04-26 10:55:30 +0200

    Update Connection.lua

--- scripts/assets/Long_Polling/Connection.lua
+++ scripts/assets/Long_Polling/Connection.lua
@@ -48,7 +48,6 @@ function Connection.new(url, id, password)
 				Url = url.."/poll/"..id,
 				Method = "GET",
 			})
-			repeat task.wait() until typeof(Data) == "table" and typeof(Data.Success) == "boolean"
 			if Data.Success == true then
 				local response = HttpService:JSONDecode(Data.Body);
 				newConnection.handlers[response.event.name](response.event.data)
